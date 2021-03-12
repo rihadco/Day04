33be5b8716d6319896df24801c28b3c5b215c1aa
rihadco <rihad.covcic@gmail.com>
2021-03-12 10:30:08 +0100

    Novi zadaci

---
+++ ex08/src/index.js
@@ -0,0 +1,22 @@
+function main(){
+    
+    // Only change code below this line
+
+    var myX = 12;
+    myX += 3;
+    var myY = 19;
+    myY += 17;
+    var myZ = 24;
+    myZ -= 20;
+    
+    // Only change code above this line
+
+    return {
+    myX,
+    myY,
+    myZ
+    };
+}
+
+console.log(main());
+module.exports = main;
---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+    
+    // Only change code below this line
+
+    var myFirstName = "Rihad";
+    var myLastName = "Covcic";
+    var myHomeTown = "Sarajevo";
+    
+    // Only change code above this line
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
