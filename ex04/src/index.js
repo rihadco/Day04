e6d51bb911568cd87e57c849250beabee2b6edc4
rihadco <rihad.covcic@gmail.com>
2021-03-12 10:38:21 +0100

    Novi zadaci

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -2,7 +2,7 @@ function main(){
     
     // Only change code below this line
 
-    var myDecimal: 8.2;
+    var myDecimal = 8.2;
     var myDecimalE = 254e-3;
     
     // Only change code above this line
